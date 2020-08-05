42f1cd6b9b824f1e451055126959902326cf2c09
zhaijingfu <zhaiblackhool@gmail.com>
2020-08-05 16:56:27 +0800

    init

---
+++ spring-boot-security/src/main/java/com/zerocamel/security/config/MySecurityConfig.java
@@ -0,0 +1,10 @@
+package com.zerocamel.security.config;
+
+/**
+ * @program: spring-boot-security
+ * @description: 安全配置
+ * @author: zeroCamel
+ * @create: 2020-08-05 16:33
+ **/
+public class MySecurityConfig {
+}
